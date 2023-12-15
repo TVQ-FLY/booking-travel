dbfefbd72c11ff835acbf939fedbc55446b50e3d
Quyền Trần Văn <tranvanquyenx4@gmail.com>
2023-12-15 16:12:05 +0700

    sp4

---
+++ sp4/Booking Travel/about.php
@@ -0,0 +1,132 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Booking Travel</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+</head>
+<body>
+<?php include('includes/header.php');?>
+<div class="banner">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> Booking Travel</h1>
+	</div>
+</div>
+
+
+<!---holiday---->
+<div class="container">
+	<div class="holiday">
+	<h3>Tổng Quan</h3>			
+	<h4 style="font-size: 30px;">
+	</br>Web booking travel là một giải pháp du lịch toàn diện cung cấp cho người dùng quyền truy cập vào một loạt các dịch vụ, bao gồm
+	vé máy bay, khách sạn, tour du lịch, cho thuê xe, và nhiều hơn nữa.
+	Các trang web này cung cấp một nền tảng thuận tiện và dễ sử dụng để người dùng có thể lên kế hoạch cho chuyến đi của mình từ bất kỳ nơi nào có kết nối internet.
+
+</br></br>Lợi ích của việc sử dụng web booking travel:
+
+</br>Thuận tiện: Người dùng có thể lên kế hoạch cho chuyến đi của mình từ bất kỳ nơi nào có kết nối internet. Họ có thể tìm kiếm các chuyến bay, khách sạn, và các dịch vụ du lịch khác theo các tiêu chí khác nhau, chẳng hạn như điểm đến, thời gian khởi hành, giá cả, và các tiện nghi.
+Image of Trang web đặt vé máy bay trực tuyếnMở trong cửa sổ mới
+Trang web đặt vé máy bay trực tuyến
+</br>Tiết kiệm thời gian: Người dùng không cần phải gọi điện hoặc đến văn phòng du lịch để đặt chỗ. Họ có thể thực hiện toàn bộ quá trình đặt chỗ trực tuyến chỉ trong vài phút.
+</br>So sánh giá: Web booking travel thường cung cấp các so sánh giá từ các nhà cung cấp khác nhau để giúp người dùng tìm được giao dịch tốt nhất.
+</br>Dễ sử dụng: Các trang web đặt vé du lịch được thiết kế để dễ sử dụng ngay cả đối với những người không có kinh nghiệm du lịch.
+</br>An toàn: Các trang web đặt vé du lịch sử dụng các biện pháp bảo mật tiên tiến để bảo vệ thông tin cá nhân và thông tin thanh toán của người dùng.
+Các loại dịch vụ du lịch có sẵn trên web booking travel:
+
+</br>Vé máy bay: Web booking travel cung cấp quyền truy cập vào một mạng lưới rộng lớn các hãng hàng không, giúp người dùng tìm thấy vé máy bay giá rẻ đến bất kỳ điểm đến nào trên thế giới.
+Khách sạn: Web booking travel cung cấp cho người dùng quyền truy cập vào một danh sách khổng lồ các khách sạn, từ các khách sạn giá rẻ đến các khách sạn sang trọng. Người dùng có thể tìm kiếm khách sạn theo điểm đến, giá cả, tiện nghi, và các tiêu chí khác.
+</br>Tour du lịch: Web booking travel cung cấp một loạt các tour du lịch, từ các tour du lịch trong ngày đến các tour du lịch nhiều ngày. Người dùng có thể tìm kiếm tour du lịch theo điểm đến, giá cả, và các hoạt động được bao gồm.
+Các hoạt động khác: Web booking travel cũng cung cấp quyền truy cập vào một loạt các dịch vụ du lịch khác, chẳng hạn như vé tham quan, dịch vụ đặt vé, và dịch vụ chuyển phát nhanh.
+Làm thế nào để sử dụng web booking travel:
+
+Để sử dụng web booking travel, người dùng cần tạo một tài khoản. Sau khi tạo tài khoản, họ có thể bắt đầu tìm kiếm các dịch vụ du lịch. Họ có thể sử dụng các công cụ tìm kiếm và bộ lọc để tìm thấy các dịch vụ phù hợp với nhu cầu của mình. Khi họ tìm thấy một dịch vụ mà họ quan tâm, họ có thể đặt chỗ trực tuyến.
+
+Lời khuyên khi sử dụng web booking travel:
+
+Sử dụng các công cụ tìm kiếm và bộ lọc để tìm thấy các dịch vụ phù hợp với nhu cầu của bạn.
+So sánh giá từ các nhà cung cấp khác nhau để tìm được giao dịch tốt nhất.
+Đọc kỹ các điều khoản và điều kiện trước khi đặt chỗ.
+	</h4>
+</div>
+			
+		
+<!-- <div><a href="package-list.php" class="view">Xem thêm Tour</a></div> -->
+</div>
+			<div class="clearfix"></div>
+	</div>
+
+
+
+<!--- routes ---->
+<div class="routes">
+	<div class="container">
+		<!-- <div class="col-md-4 routes-left wow fadeInRight animated" data-wow-delay=".5s">
+			<div class="rou-left">
+				<a href="#"><i class="fa fa-ticket"></i></a>
+			</div>
+			<div class="rou-rgt wow fadeInDown animated" data-wow-delay=".5s">
+				<h3>80000</h3>
+				<p>Góp Ý</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 routes-left">
+			<div class="rou-left">
+				<a href="#"><i class="fa fa-user"></i></a>
+			</div>
+			<div class="rou-rgt">
+				<h3>1900</h3>
+				<p>Người dùng quay lại</p>
+			</div>
+				<div class="clearfix"></div>
+		</div> 
+		<div class="col-md-4 routes-left wow fadeInRight animated" data-wow-delay=".5s">
+			<div class="rou-left">
+				<a href="#"><i class="fa fa-ticket"></i></a>
+			</div>
+			<div class="rou-rgt">
+				<h3>7,00,00,000+</h3>
+				<p>Booking</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div> -->
+	</div>
+</div>
+
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>			
+<!-- //write us -->
+
+</body>
+</html>
